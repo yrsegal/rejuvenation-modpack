55885d0a061e80f3c069421a1750985dab7e77a5
Yoni Segal <yrsegal@gmail.com>
2025-06-05 18:02:59 -0400

    for ultra burst too why not

--- PredictedMegaMoveTypes.rb
+++ PredictedMegaMoveTypes.rb
@@ -17,6 +17,18 @@ class PokeBattle_Move
         attacker.ability = prevAbility
         return ret
       end
+    elsif !attacker.isUltra? && attacker.hasUltra?
+      side=(battle.pbIsOpposing?(attacker.index)) ? 1 : 0
+      owner=battle.pbGetOwnerIndex(attacker.index)
+      if battle.ultraBurst[side][owner] == attacker.index
+        attacker.pokemon.makeUltra
+        prevAbility = attacker.ability
+        attacker.ability = attacker.pokemon.ability
+        ret = predictedmegamovetypes_old_pbType(attacker, type)
+        attacker.pokemon.makeUnultra
+        attacker.ability = prevAbility
+        return ret
+      end
     end
     return predictedmegamovetypes_old_pbType(attacker, type)
   end
