292cc1c95b5ccece9bfe6bdd8538a6fb0735ce14
Yoni Segal <yrsegal@gmail.com>
2025-07-13 14:45:47 -0400

    actually make multiple reverse candies work

--- ReverseCandyMultiple.rb
+++ ReverseCandyMultiple.rb
@@ -1,1 +1,13 @@
 ItemHandlers::MultipleAtOnce.push(:REVERSECANDY)
+ItemHandlers::UseOnPokemon.add(:REVERSECANDY,proc{|item,pokemon,scene,amount=1|
+   if pokemon.level==1 || (pokemon.isShadow? rescue false)
+     scene.pbDisplay(_INTL("It won't have any effect."))
+     next false, 0
+   else
+     used = [amount,pokemon.level - 1].min
+     pbChangeLevel(pokemon,pokemon.level-used,scene)
+     pokemon.changeHappiness("badcandy")
+     scene.pbHardRefresh
+     next true, used
+   end
+})
