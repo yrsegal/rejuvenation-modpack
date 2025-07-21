03efd06dab3e51c588395c3c9d8a5c696ece3d7f
Yoni Segal <yrsegal@gmail.com>
2025-07-21 14:54:43 -0400

    funny torchic event

---
+++ TorchicEvent.rb
@@ -0,0 +1,36 @@
+
+Variables[:QuestXenogene] = 620
+
+InjectionHelper.defineMapPatch(203) { |map| # Pokestar Studios Interiors
+  rawev = RPG::Event.new(86, 10)
+  rawev.pages.push(RPG::Event::Page.new)
+  rawev.id = map.events.values.max { |a, b| a.id <=> b.id }.id + 1
+
+  rawev.pages[1].condition.self_switch_valid = true
+  rawev.pages[1].condition.self_switch_ch = "A"
+  rawev.pages[1].trigger = 3 # autorun
+  rawev.pages[1].list = InjectionHelper.parseEventCommands(
+    [:Script, "$game_map.data[86,9,2] = 0"], # Deletes the torchic
+    :EraseEvent,
+    :Done) 
+
+  rawev.pages[0].condition.variable_valid = true
+  rawev.pages[0].condition.variable_id = Variables[:QuestXenogene]
+  rawev.pages[0].condition.variable_value = 72
+  rawev.pages[0].trigger = 0 # action button
+  rawev.pages[0].list = InjectionHelper.parseEventCommands(
+    [:PlaySoundEvent, '255Cry', 80, 100],
+    [:ShowText, "TORCHIC: Torchic Tor!"],
+    [:ShowAnimation, 18, EXCLAMATION_ANIMATION_ID], # Dyre (visual location)
+    [:ShowText,          "DYRE: Oh, yeah. That Torchic loves pretending"],
+    [:ShowTextContinued, "to be a doll. Don't know why."],
+    [:ShowText,          "I've been meaning to find a home for the fella"],
+    [:ShowTextContinued, "anyway. Take it, I insist!"],
+    [:Script, "$game_map.data[86,9,2] = 0"], # Deletes the torchic
+    [:Script, "Kernel.pbAddPokemon(:TORCHIC,10)"],
+    [:ControlSelfSwitch, "A", true],
+    :Done)
+
+  map.events[rawev.id] = rawev
+  next true
+}
