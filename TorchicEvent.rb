3311d6090ec9a5949aadf61c273adf7df9aa2c2b
Yoni Segal <yrsegal@gmail.com>
2025-07-21 15:02:31 -0400

    add dialogue to hint at torchic

--- TorchicEvent.rb
+++ TorchicEvent.rb
@@ -3,24 +3,27 @@ Variables[:QuestXenogene] = 620
 
 InjectionHelper.defineMapPatch(203) { |map| # Pokestar Studios Interiors
   rawev = RPG::Event.new(86, 10)
-  rawev.pages.push(RPG::Event::Page.new)
+  rawev.pages.push(RPG::Event::Page.new, RPG::Event::Page.new)
   rawev.id = map.events.values.max { |a, b| a.id <=> b.id }.id + 1
 
-  rawev.pages[1].condition.self_switch_valid = true
-  rawev.pages[1].condition.self_switch_ch = "A"
-  rawev.pages[1].trigger = 3 # autorun
-  rawev.pages[1].list = InjectionHelper.parseEventCommands(
+  rawev.pages[2].condition.self_switch_valid = true
+  rawev.pages[2].condition.self_switch_ch = "A"
+  rawev.pages[2].trigger = 3 # autorun
+  rawev.pages[2].list = InjectionHelper.parseEventCommands(
     [:Script, "$game_map.data[86,9,2] = 0"], # Deletes the torchic
     :EraseEvent,
     :Done) 
 
-  rawev.pages[0].condition.variable_valid = true
-  rawev.pages[0].condition.variable_id = Variables[:QuestXenogene]
-  rawev.pages[0].condition.variable_value = 72
-  rawev.pages[0].trigger = 0 # action button
-  rawev.pages[0].list = InjectionHelper.parseEventCommands(
+  rawev.pages[1].condition.variable_valid = true
+  rawev.pages[1].condition.variable_id = Variables[:QuestXenogene]
+  rawev.pages[1].condition.variable_value = 72
+  rawev.pages[1].trigger = 0 # action button
+  rawev.pages[1].list = InjectionHelper.parseEventCommands(
+    [:ShowText, "It's a stuffed Torchic doll!"],
+    [:ShowText, "It's warm..."],
     [:PlaySoundEvent, '255Cry', 80, 100],
     [:ShowText, "TORCHIC: Torchic Tor!"],
+    [:ShowAnimation, :Player, EXCLAMATION_ANIMATION_ID],
     [:ShowAnimation, 18, EXCLAMATION_ANIMATION_ID], # Dyre (visual location)
     [:ShowText,          "DYRE: Oh, yeah. That Torchic loves pretending"],
     [:ShowTextContinued, "to be a doll. Don't know why."],
@@ -29,6 +32,12 @@ InjectionHelper.defineMapPatch(203) { |map| # Pokestar Studios Interiors
     [:Script, "$game_map.data[86,9,2] = 0"], # Deletes the torchic
     [:Script, "Kernel.pbAddPokemon(:TORCHIC,10)"],
     [:ControlSelfSwitch, "A", true],
+    :Done)
+
+  rawev.pages[0].trigger = 0 # action button
+  rawev.pages[0].list = InjectionHelper.parseEventCommands(
+    [:ShowText, "It's a stuffed Torchic doll!"],
+    [:ShowText, "It's warm..."],
     :Done)
 
   map.events[rawev.id] = rawev
