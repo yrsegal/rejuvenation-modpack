467fd9612befcefcd9f149ba00ff9a37e8fbcc7c
Yoni Segal <yrsegal@gmail.com>
2025-07-24 10:14:15 -0400

    fix accidental buff to prism power

--- CleanerPrismPower.rb
+++ CleanerPrismPower.rb
@@ -18,16 +18,13 @@ class PokeBattle_Battler
       end
     end
   end
-
-  alias :cleanerprismpower_old_pbInitPokemon :pbInitPokemon
-
-  def pbInitPokemon(pkmn, pkmnIndex)
-    cleanerprismpower_old_pbInitPokemon(pkmn, pkmnIndex)
-    # My soul weeps in pain - this if statement has an !
-    if @pokemon.prismPower
-      @pokemon.prismPower = false
-    end
-  end
 end
 
+module PokeBattle_BattleCommon
+  alias :cleanerprismpower_old_pbStorePokemon :pbStorePokemon
 
+  def pbStorePokemon(pokemon)
+    @pokemon.prismPower = false if @pokemon.prismPower && pokemon.species == :DELPHOX && pokemon.form == 1
+    cleanerprismpower_old_pbStorePokemon(pkmn, pkmnIndex)
+  end
+end
