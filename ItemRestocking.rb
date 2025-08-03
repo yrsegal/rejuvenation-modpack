655c665691f93638ed03a6a8329d53cc7f3ce2e9
Yoni Segal <yrsegal@gmail.com>
2025-08-02 22:06:07 -0400

    add a hook for no restocking

--- ItemRestocking.rb
+++ ItemRestocking.rb
@@ -1,12 +1,16 @@
+$NO_RESTOCKING = false
+
 class PokeBattle_Battle
   alias :restocking_old_pbEndOfBattle :pbEndOfBattle
 
   def pbEndOfBattle(*args, **kwargs)
-    for i in $Trainer.party
-      itemToCheck = i.itemReallyInitialHonestlyIMeanItThisTime
-      if !itemToCheck.nil? && $PokemonBag.pbQuantity(itemToCheck) > 0 && i.itemInitial.nil?
-        $PokemonBag.pbDeleteItem(itemToCheck)
-        i.itemInitial = itemToCheck
+    unless $game_switches[:NotPlayerCharacter] || $NO_RESTOCKING
+      for i in $Trainer.party
+        itemToCheck = i.itemReallyInitialHonestlyIMeanItThisTime
+        if !itemToCheck.nil? && $PokemonBag.pbQuantity(itemToCheck) > 0 && i.itemInitial.nil?
+          $PokemonBag.pbDeleteItem(itemToCheck)
+          i.itemInitial = itemToCheck
+        end
       end
     end
     return restocking_old_pbEndOfBattle(*args, **kwargs)
