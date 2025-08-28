68d8f4bc3d3dacecee0e5bafeb1f74473a4f47d7
Yoni Segal <yrsegal@gmail.com>
2025-08-27 20:09:15 -0400

    change name to garufan pattern

--- FixVivillonForms.rb
+++ FixVivillonForms.rb
@@ -1,7 +1,7 @@
 $cache.pkmn[:VIVILLON].formInit = "proc{rand(10)}" # For serialization reasons, these procs are stored as strings
 
-["Astral Pattern",  "Dream Pattern",  "Tropics Pattern",  "Radiant Pattern",     "Snowy Pattern", 
- "Spotted Pattern", "Garufa Pattern", "Sinister Pattern", "Celebratory Pattern", "Poilethal Pattern"].each_with_index do |pattern, idx|
+["Astral Pattern",  "Dream Pattern",   "Tropics Pattern",  "Radiant Pattern",     "Snowy Pattern", 
+ "Spotted Pattern", "Garufan Pattern", "Sinister Pattern", "Celebratory Pattern", "Poilethal Pattern"].each_with_index do |pattern, idx|
   $cache.pkmn[:VIVILLON].formData[pattern] = {} if idx > 0
   $cache.pkmn[:VIVILLON].forms[idx] = pattern
 end
