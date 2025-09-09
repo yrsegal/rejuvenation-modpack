cab5188115347746daca3dc431ce95a2dea31e2c
Yoni Segal <yrsegal@gmail.com>
2025-09-09 14:32:00 -0400

    fix misnamed hidden items

--- ItemRadar.rb
+++ ItemRadar.rb
@@ -6,6 +6,17 @@ begin
   raise "Missing dependencies for mod #{__FILE__}, cannot load" unless missing.empty?
 end
 
+### Fix misnamed events
+
+InjectionHelper.defineMapPatch(58, 8) { |evt| evt.name = "HiddenItem" } # East Gearen, pokeball
+InjectionHelper.defineMapPatch(119, 17) { |evt| evt.name = "HiddenItem" } # Carotos Mountain, blast powder
+InjectionHelper.defineMapPatch(250, 70) { |evt| evt.name = "HiddenItem" } # Marble Mansion, silk scarf
+
+InjectionHelper.defineMapPatch(48, 9) { |evt| evt.name = "EV009" } # East Gearen interiors, poison barb misnamed as hidden
+InjectionHelper.defineMapPatch(75, 13) { |evt| evt.name = "EV013" } # Evergreen Island, primarium misnamed as hidden
+
+### Done with misnamed events
+
 ### MODDED/
 Events.onMapChange+=proc {
   $game_screen.itemRadar_updateItemRadar
