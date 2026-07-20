bce629d2f76b305be2a54cf15da0bf9970492900
DeltaJordan <marine.jordan64@proton.me>
2026-07-19 20:05:17 -0500

    Merge pull request #1 from DeltaJordan/fix/oricorio-hold-nectar-crash
    Fix OricorioHoldNectar causing an error popup.

--- Mods/OricorioHoldNectar.rb
+++ Mods/OricorioHoldNectar.rb
@@ -5,17 +5,18 @@ class ItemData
   attr_writer :flags
 end
 
+ModCacheInjection.hook(:pkmn) {
+  {
+    "Baile Style" => :REDNECTAR,
+    "Pom-Pom Style" => :YELLOWNECTAR,
+    "Pa'u Style" => :PINKNECTAR,
+    "Sensu Style" => :PURPLENECTAR,
+  }.each do |key,value|
+    oricorio = $cache.pkmn[:ORICORIO, key].flags
 
-{
-  "Baile Style" => :REDNECTAR,
-  "Pom-Pom Style" => :YELLOWNECTAR,
-  "Pa'u Style" => :PINKNECTAR,
-  "Sensu Style" => :PURPLENECTAR,
-}.each do |key,value|
-  oricorio = $cache.pkmn[:ORICORIO, key].flags
-
-  oricorio[:WildItemCommon] = value
-  oricorio[:WildItemUncommon] = value
-  oricorio[:WildItemRare] = value
-end
+    oricorio[:WildItemCommon] = value
+    oricorio[:WildItemUncommon] = value
+    oricorio[:WildItemRare] = value
+  end
+}
 
