e0745fe2fa00f6e8ecebb05bd0672f844bcd5713
Jordan Marine <marine.jordan64@proton.me>
2026-07-19 19:32:40 -0500

    Fixed $cache.pkmn not initialized before attempting to set the held items for Oricorio to their respective nectar.

--- Mods/OricorioHoldNectar.rb
+++ Mods/OricorioHoldNectar.rb
@@ -5,17 +5,27 @@ class ItemData
   attr_writer :flags
 end
 
+class PokemonLoad
+  alias :wires_startPlayingSaveFile_old :startPlayingSaveFile
 
-{
-  "Baile Style" => :REDNECTAR,
-  "Pom-Pom Style" => :YELLOWNECTAR,
-  "Pa'u Style" => :PINKNECTAR,
-  "Sensu Style" => :PURPLENECTAR,
-}.each do |key,value|
-  oricorio = $cache.pkmn[:ORICORIO, key].flags
+  def startPlayingSaveFile(*args, **kwargs)
+    ret = wires_startPlayingSaveFile_old(*args, **kwargs)
+    
+    {
+      "Baile Style" => :REDNECTAR,
+      "Pom-Pom Style" => :YELLOWNECTAR,
+      "Pa'u Style" => :PINKNECTAR,
+      "Sensu Style" => :PURPLENECTAR,
+    }.each do |key,value|
+      oricorio = $cache.pkmn[:ORICORIO, key].flags
 
-  oricorio[:WildItemCommon] = value
-  oricorio[:WildItemUncommon] = value
-  oricorio[:WildItemRare] = value
+      oricorio[:WildItemCommon] = value
+      oricorio[:WildItemUncommon] = value
+      oricorio[:WildItemRare] = value
+    end
+
+    puts "Loaded OricorioHoldNectar."
+
+    return ret
+  end
 end
-
